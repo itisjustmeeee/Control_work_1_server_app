9e79a1c5bb6d714730663736504f966484ed13ce
_itisjustme _ <guzairovaolga21@gmail.com>
2026-02-14 19:03:15 +0300

    done task 2.1

---
+++ task 2.1/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Feedback(BaseModel):
+    name: str
+    message: str
+
+message_bd = []
+
+@app.get('/')
+async def get_messages():
+    return message_bd
+
+@app.post('/feedback')
+async def get_message(mes: Feedback):
+    message_bd.append({"name": mes.name, "message": mes.message})
+    return {"message": f"Feedback received. Thank you, {mes.name}"}
